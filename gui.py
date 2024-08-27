fd8b24d9ace0fb3a5d31f1a4d257970ad5a2f918
46910891922k23 <xbooi023@student.wethinkcode.co.za>
2024-08-27 14:16:35 +0200

    adding functionality on gui, test currently failing

--- gui.py
+++ gui.py
@@ -1,12 +1,15 @@
 import tkinter as tk
+from main_file import *
 
 def user_interfase():
     root= tk.Tk()
 
     root.geometry("500x500")
     root.title("Welcome page")
-
+    root.baseName("xolela")
     label = tk.Label(root, text="Sign up/Sign in",font=("Bold", 15))
+    username = tk(user_input())
+    username_botton = tk.Button(root,username)
     label.pack()
     text_box = tk.Text(root, height=2,font=("bold",16))
     text_box.pack(padx=10)
--- main_file.py
+++ main_file.py
@@ -2,7 +2,7 @@ def main():
     user_login()
     sign_up()
     user_name()
-# user log in or sing up
+
 
 def user_login():
     x = input('do you have a account YES / NO :').upper()
@@ -20,10 +20,12 @@ def user_input(password,username):
     pass   
 
 def sign_up():
+    #sign up details need to be added to json file
     pass_list = []
     pass_word = input("create a password")
     pass_list.append(pass_word)
     print(f"your password is  : {pass_word}")
+
 
 #creating username 
 def user_name():
@@ -33,7 +35,7 @@ def user_name():
     x.append(y)   
      
 # validate if the user input is valid and the pass word is strong
-main()
+
 
 
 
@@ -48,3 +50,4 @@ main()
 
 
 # Overwritte to a file to keep the data for futer login
+main()
